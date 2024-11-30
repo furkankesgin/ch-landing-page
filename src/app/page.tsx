e505cdfc1af538c530c9ea528c990fe1b40ba75e
cccaaannn <can.kurt.aa@gmail.com>
2024-11-30 20:06:18 +0300

    project scaffold added

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+const AppHome = async () => {
+  return (
+    <Box>
+      hello world
+    </Box>
+  );
+};
+
+export default AppHome;
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export interface NpmData {
+  version: string;
+}
