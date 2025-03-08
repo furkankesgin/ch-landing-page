8d03de9ae2c97b36d52becd5fb34de9a60c27c0a
cccaaannn <can.kurt.aa@gmail.com>
2025-03-09 00:32:37 +0300

    types fixed

---
+++ src/data/types/image-globals.d.ts
@@ -0,0 +1,24 @@
+declare module "*.jpg" {
+	const jpgValue: string;
+	export default jpgValue;
+}
+
+declare module "*.jpeg" {
+	const jpegValue: string;
+	export default jpegValue;
+}
+
+declare module "*.png" {
+	const pngValue: string;
+	export default pngValue;
+}
+
+declare module "*.gif" {
+	const gifValue: string;
+	export default gifValue;
+}
+
+declare module "*.svg" {
+	const svgValue: string;
+	export default svgValue;
+}
